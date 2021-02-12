cdce6fbd71d3e2ddbb2afdb6e524101561b884d0
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-12 09:43:51 -0700

    Add app main routes without template file names

--- app/app.py
+++ app/app.py
@@ -11,3 +11,76 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ap
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = "secret"
 db = SQLAlchemy(app)
+
+# db models here
+
+
+
+@app.route('/')
+def homePage():
+    return render_template('index.html')
+
+
+@app.route('/success')
+def success():
+    return render_template()
+
+
+@app.route('/contact-us')
+def contactUs():
+    return render_template()
+
+
+@app.route('/about-us')
+def aboutUs():
+    return render_template()
+
+
+@app.route('/account')
+def account():
+    return render_template()
+
+
+@app.route('/auth-page')
+def authPage():
+    return render_template()
+
+
+@app.route('/wish-list')
+def wishList():
+    return render_template()
+
+
+@app.route('/shopping-lists')
+def shoppingLists():
+    return render_template()
+
+
+@app.route('/shopping-list/<id>', method=['GET'])
+def shoppingList(id):
+    return render_template()
+
+
+@app.route('/edit-list/<id>', method=['GET', 'POST'])
+def editList(id):
+    return render_template()
+
+
+@app.route('/create-list', method=['GET', 'POST'])
+def createList():
+    return render_template()
+
+
+@app.route('/delete-list', method=['POST'])
+def deleteList():
+    return render_template()
+
+
+@app.route('/edit-item', method=['GET', 'POST'])
+def editItem():
+    return render_template()
+
+
+@app.route('/delete-item', method=['POST'])
+def deleteItem():
+    return render_template()
