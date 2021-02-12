02f59d5024c21d788e06c3aecacbf7121e40495a
rhyusarmiento <rhyu.sar.15@gmail.com>
2021-02-12 09:41:06 -0700

    Add app config

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+app = Flask(__name__)
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = "secret"
+db = SQLAlchemy(app)
